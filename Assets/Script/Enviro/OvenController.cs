274ef2e495e9e2dace4578771bb95dd01fc61ff4
Barbrasia <bbojanowska@edu.cdv.pl>
2023-06-15 22:12:15 +0200

    RollingPin Tag but its not working with oven

--- Assets/Script/Enviro/OvenController.cs
+++ Assets/Script/Enviro/OvenController.cs
@@ -5,11 +5,24 @@ using DG.Tweening;
 
 public class OvenController : MonoBehaviour
 {
+    public GameObject oven;
+    public GameObject cake;
+
     public GameObject door;
     public DOTweenAnimation doorAnimation;
 
     public GameObject tray;
     public DOTweenAnimation trayAnimation;
+
+    private void Awake()
+    {
+        Rigidbody rb = cake.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+        }
+    }
 
     private void Start()
     {
@@ -19,10 +32,18 @@ public class OvenController : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        if (other.CompareTag("RollingPin") && Input.GetKey(KeyCode.E))
         {
             doorAnimation.DOPlay();
             trayAnimation.DOPlay();
+
+            StartCoroutine(Finished());
         }
+    }
+
+    private IEnumerator Finished()
+    {
+        yield return new WaitForSeconds(7);
+        GameObject.Destroy(oven);
     }
 }
